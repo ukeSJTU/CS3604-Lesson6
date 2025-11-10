b360cccf4407575203fb3d1c84df99aabc1c1ad8
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-11-10 14:06:14 +0800

    fix(tests): correct expected result in MainTest assertion

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -1,6 +1,6 @@
 package com;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +10,7 @@ class MainTest {
 	void test() {
 		Main main = new Main();
 		int result = main.add(1, 1);
-		assertEquals(1, result);
+		assertEquals(2, result);
 	}
 
 }
