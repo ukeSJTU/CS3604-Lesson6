7a1a29b1cae85f4acac7d6a5818d83f0a2a8698e
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-11-10 14:44:16 +0800

    refactor(ResourceLeakExample): comment out hardcoded password in main3 method

--- src/main/java/com/ResourceLeakExample.java
+++ src/main/java/com/ResourceLeakExample.java
@@ -34,8 +34,9 @@ public class ResourceLeakExample {
    }
 	
 	public void main3() {
-		String password = "12345"; 
-        System.out.println(password);
+		// String password = "12345"; 
+        // System.out.println(password);
+        System.out.println("Hello world");
     }
 
 }
